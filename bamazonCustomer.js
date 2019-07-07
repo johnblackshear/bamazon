2cda0904593d1c7a60b086885f24e66588868949
John Blackshear <swjaybee@gmail.com>
2019-07-06 19:03:29 -0500

    init

---
+++ bamazonCustomer.js
@@ -0,0 +1,20 @@
+var mysql = require("mysql");
+
+var inquire = require("inquire");
+
+// mysql connection parameters
+
+ var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "",
+    database: "bamazon"
+  });
+  
