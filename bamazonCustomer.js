a360e6d706a33216685407f11fe554f2bbc093b5
John Blackshear <swjaybee@gmail.com>
2019-07-08 20:46:17 -0500

    final bamazonCustomer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,84 +17,7 @@ var inquirer = require('inquirer');
     password: "joesmells007",
     database: "bamazon"
   });
-
-  /*connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    //startPromt();
-    showInventory();
-
-  });
-
-
-/*
-  function startPromt(){
-      inquirer.prompt([{
-          type:"confirm",
-          name:"confirm",
-          message:"welcome would you like to veiw our invetory?",
-          default:true
-      }]).then(function(user){
-          if (user.confirm === true){
-              startBamazon();
-          }else{
-              console.log("Thanks for visiting!");
-          }
-      });
-  }
-// user prompt for item and quantity
-  function promptUserSelection(){
-
-    inquirer.prompt([
-    {
-        name: 'item_id',
-        type: 'input',
-		message: 'Please enter the Item ID which you would like to purchase.',
-			
-
-    },
-    {
-        type: 'input',
-        name: 'quantity',
-        message: 'Please give an amount needed.',   
-        filter: Number
-    },
-
-    
-    ]).then(function(input){
-        var item = input.item_id;
-        var quantity = input.quantity;
-
-        var que = "SELECT * FROM products WHERE? ";
-
-        connection.query(que, {item_id: item}, function(err, data){
-            if (err) throw err;
-
-            if(data.length === 0){
-                console.log("ERROR: invalid id");
-                showInventory();
-            }else
-            var productData = data[0];
-
-            if (quantity <= productData.stock_quantity){
-                console.log("The requested item is in stock");
-
-                var upadateQue = "UPDATE products SET stock_quantity = " + (productData.stock_quantity - quantity) + "WHERE item_id =" + item;
-
-                connection.query(upadateQue, function(err, data){
-                    if (err) throw err; 
-                    console.log("Your order has been place" + productData.price * quantity);
-                    connection.end();
-                })
-            }else{
-                console.log("sorry, the product" + productData.product_name + "is not in stock");
-                console.log("please update your order.");
-                showInventory();
-            }
-        })
-    })
-
-  }*/
+//----------- function to display information in a cli table ------------------
 
 function showInventory(){
       
@@ -106,7 +29,6 @@ function showInventory(){
         console.log("Availible Inventory");
         console.log("------------------------");
 
-        //promptUserSelection();
      
       var table = new Table({
         head: ['Product Id', 'Product Name', 'Price', 'QTY'], 
@@ -122,6 +44,8 @@ function showInventory(){
     promptUserSelection();
 });
 };
+
+// ----------- function and prompt to for user to select a product using item_id -----------
 
 var  promptUserSelection = function(){
 
@@ -140,6 +64,9 @@ var  promptUserSelection = function(){
                console.log("Item is not in inventory, please select from listed items.");
             promptUserSelection();
            }else{
+// ---------------- show product name when item_id is selected -----------------
+               console.log("You have selected: " + res[0].product_name);
+// ----------- prompt for quantity selection -------------------------
             inquirer.prompt({
                 name: "quantity",
                 type: "input",
@@ -152,8 +79,21 @@ var  promptUserSelection = function(){
                 promptUserSelection();
                 }else{
                     console.log("there is enough in stock");
+                    console.log(res[0].product_name);
+                    console.log("Your total is: " + "$" + res[0].price * selectedQty);
+
+// ------------------ update the qauntity in table --------------------------
+                var updateQty = res[0].stock_quantity - selectedQty;
+                connection.query(
+                    "UPDATE products SET stock_quantity = " + updateQty + " WHERE item_id = " + res[0].item_id,
+                    function(err, resUpdate) {
+                        if (err) throw err;
+                        console.log("Your Order has been Processed. Thank You for your service..!");
+                        connection.end();
+                        }
+                    );   
                 }
-            })
+            });
            }
         });
     });
